3b36a09ff5b28b48260b2db84ee260782d8a7926
Meghana Purijala <38654746+meghana-1998@users.noreply.github.com>
2023-10-08 10:39:43 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+
+    
+    api: { 
+
+        invokeUrl: 'https://hazxk80978.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
+
+    } 
+
+};
